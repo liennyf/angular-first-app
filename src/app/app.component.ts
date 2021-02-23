af01c6c864d5d663cd4c5d030218ffa127f582f6
liennyf <lienny127@gmail.com>
2021-02-23 19:56:58 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  hasil = 0;
+  a=0;
+  b=0;
+  tulisanOpen:String;
+  
+  item = [
+    {nama : "Jeruk", harga:15000, satuan : "kg"},
+    {nama : "Apel", harga:5000, satuan : "kg"}
+  ]
+
+  tampil : boolean = true;
+  
+  OPEN(){
+    this.tulisanOpen = "TOKO DIBUKA";
+    alert("DIBUKA");
+  }
+
+  values = "";
+  onKey(event:any){
+    this.values += event.target.value + '|'
+  }
 }
